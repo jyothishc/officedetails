4cc7321b220642d0f4c3c23a7dba7b0fd33c42b6
jyotheesh k <jyothishc30@gmail.com>
2022-09-12 16:50:20 +0530

    new project create

---
+++ src/main/java/com/springboot/OfficedetailsApplication.java
@@ -0,0 +1,13 @@
+package com.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OfficedetailsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OfficedetailsApplication.class, args);
+	}
+
+}
